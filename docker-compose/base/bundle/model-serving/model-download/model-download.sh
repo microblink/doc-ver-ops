a9a26085b07a1851c7701b6bd0cfdc42620fdec3
Kresimir Jelic <kresimir.jelic@microblink.com>
2024-06-13 11:58:16 +0200

    Fixed model download

--- docker-compose/base/bundle/model-serving/model-download/model-download.sh
+++ docker-compose/base/bundle/model-serving/model-download/model-download.sh
@@ -2,9 +2,9 @@
 set -e;
 rclone copy src://identity-enc-models-public/models/6478fcb410dcce6d3b037199 /models/6478fcb410dcce6d3b037199 -vv;
 echo '6478fcb410dcce6d3b037199 downloaded';
-rclone copy src://identity-enc-models/models/6647158183f1d72d549f88e4 /models/6647158183f1d72d549f88e4 -vv;
+rclone copy src://identity-enc-models-public/models/6647158183f1d72d549f88e4 /models/6647158183f1d72d549f88e4 -vv;
 echo '6647158183f1d72d549f88e4 downloaded';
-rclone copy src://identity-enc-models/models/664f60da83f1d72d549f88f3 /models/664f60da83f1d72d549f88f3 -vv;
+rclone copy src://identity-enc-models-public/models/664f60da83f1d72d549f88f3 /models/664f60da83f1d72d549f88f3 -vv;
 echo '664f60da83f1d72d549f88f3 downloaded';
-rclone copy src://identity-enc-models/models/664f60f083f1d72d549f88f4 /models/664f60f083f1d72d549f88f4 -vv;
+rclone copy src://identity-enc-models-public/models/664f60f083f1d72d549f88f4 /models/664f60f083f1d72d549f88f4 -vv;
 echo '664f60f083f1d72d549f88f4 downloaded';
