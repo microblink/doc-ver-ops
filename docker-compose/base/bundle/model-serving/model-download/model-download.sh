db3511917285b66251e2654d30ae83fbf83498b0
Petar Pavao Lacić <petar.pavao.lacic@microblink.com>
2024-07-17 15:28:48 +0200

    Fixed model bucket name.

--- docker-compose/base/bundle/model-serving/model-download/model-download.sh
+++ docker-compose/base/bundle/model-serving/model-download/model-download.sh
@@ -10,9 +10,9 @@ echo '6478fcb410dcce6d3b037199 downloaded';
 #
 # Standard
 #
-rclone copy src://mlp-models-prod/models/667a8f0b76c3ea3a32fcd565 /models/667a8f0b76c3ea3a32fcd565 -vv;
+rclone copy src://identity-enc-models-public/models/667a8f0b76c3ea3a32fcd565 /models/667a8f0b76c3ea3a32fcd565 -vv;
 if [ ! -d /models/667a8f0b76c3ea3a32fcd565 ]; then echo 'failed to clone model 667a8f0b76c3ea3a32fcd565'; exit 1; fi;
 echo '667a8f0b76c3ea3a32fcd565 downloaded';
-rclone copy src://mlp-models-prod/models/6684168e04248f708c1e0c93 /models/6684168e04248f708c1e0c93 -vv;
+rclone copy src://identity-enc-models-public/models/6684168e04248f708c1e0c93 /models/6684168e04248f708c1e0c93 -vv;
 if [ ! -d /models/6684168e04248f708c1e0c93 ]; then echo 'failed to clone model 6684168e04248f708c1e0c93'; exit 1; fi;
 echo '6684168e04248f708c1e0c93 downloaded';
