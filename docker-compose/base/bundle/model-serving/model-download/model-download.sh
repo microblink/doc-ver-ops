1923a80d2fa8479fdd24c326514fa6fc7396d8ad
petarpavaolacic <108792194+petarpavaolacic@users.noreply.github.com>
2024-09-27 10:05:49 +0200

    Updated docker-compose.

--- docker-compose/base/bundle/model-serving/model-download/model-download.sh
+++ docker-compose/base/bundle/model-serving/model-download/model-download.sh
@@ -3,16 +3,16 @@ set -e;
 #
 # VA
 #
-rclone copy src://identity-enc-models-public/models/6478fcb410dcce6d3b037199 /models/6478fcb410dcce6d3b037199 -vv;
-if [ ! -d /models/6478fcb410dcce6d3b037199 ]; then echo 'failed to clone model 6478fcb410dcce6d3b037199'; exit 1; fi;
-echo '6478fcb410dcce6d3b037199 downloaded';
+rclone copy src://identity-enc-models-public/models/6687ad3b04248f708c1e0c95 /models/6687ad3b04248f708c1e0c95 -vv;
+if [ ! -d /models/6687ad3b04248f708c1e0c95 ]; then echo 'failed to clone model 6687ad3b04248f708c1e0c95'; exit 1; fi;
+echo '6687ad3b04248f708c1e0c95 downloaded';
 
 #
 # Standard
 #
-rclone copy src://identity-enc-models-public/models/66a0a678fb85edd8517b6af5 /models/66a0a678fb85edd8517b6af5 -vv;
-if [ ! -d /models/66a0a678fb85edd8517b6af5 ]; then echo 'failed to clone model 66a0a678fb85edd8517b6af5'; exit 1; fi;
-echo '66a0a678fb85edd8517b6af5 downloaded';
-rclone copy src://identity-enc-models-public/models/6684168e04248f708c1e0c93 /models/6684168e04248f708c1e0c93 -vv;
-if [ ! -d /models/6684168e04248f708c1e0c93 ]; then echo 'failed to clone model 6684168e04248f708c1e0c93'; exit 1; fi;
-echo '6684168e04248f708c1e0c93 downloaded';
+rclone copy src://identity-enc-models-public/models/66e2b36788703787163f0ecf /models/66e2b36788703787163f0ecf -vv;
+if [ ! -d /models/66e2b36788703787163f0ecf ]; then echo 'failed to clone model 66e2b36788703787163f0ecf'; exit 1; fi;
+echo '66e2b36788703787163f0ecf downloaded';
+rclone copy src://identity-enc-models-public/models/66b61951fb85edd8517b6af6 /models/66b61951fb85edd8517b6af6 -vv;
+if [ ! -d /models/66b61951fb85edd8517b6af6 ]; then echo 'failed to clone model 66b61951fb85edd8517b6af6'; exit 1; fi;
+echo '66b61951fb85edd8517b6af6 downloaded';
